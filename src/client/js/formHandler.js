366124f237c918fcafc96e56f8f1c5d01855499a
mohahdy <mohahdyc@hotmail.com>
2022-01-12 01:47:31 +0200

    fixed the issue regarding reading api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,29 +1,44 @@
 function handleSubmit(event) {
     event.preventDefault()
     let fetchedData;
-    const formdata = new FormData();
-    formdata.append("key", process.env.API_KEY);
-    formdata.append("url", "https://www.nytimes.com/2022/01/11/us/politics/biden-filibuster-voting-rights.html");
-    formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+    // const fetch = require('node-fetch')
 
-    const requestOptions = {
-        method: 'POST',
-        body: formdata,
-        redirect: 'follow'
-    };
-    fetch("https://api.meaningcloud.com/sentiment-2.1?", requestOptions)
-        .then(response => (response.json()))
-        .then((res) => ( function (res) {
-                console.log("client side res: ", res)
-                document.getElementById('agreement').innerHTML = "Agreement : " + res.agreement
-                document.getElementById('confidence').innerHTML = "Confidence : " + res.confidence
-                document.getElementById('irony').innerHTML = "Irony:  : " + res.irony
-                document.getElementById('model').innerHTML = "Model : " + res.model
-                document.getElementById('score_tag').innerHTML = "Score Tag : " + res.score_tag
+    const url = "https://www.nytimes.com/2022/01/11/us/politics/biden-filibuster-voting-rights.html"
+    // const formdata = new FormData();
+    // formdata.append("key", process.env.API_KEY);
+    // formdata.append("url", "https://www.nytimes.com/2022/01/11/us/politics/biden-filibuster-voting-rights.html");
+    // formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+    // console.log("formdata : ", formdata);
+    // const requestOptions = {
+    //     method: 'POST',
+    //     body: formdata,
+    //     redirect: 'follow'
+    // };
+    // console.log("requestOptions: ", requestOptions);
+    fetch("https://api.meaningcloud.com/sentiment-2.1?"+`key=${ process.env.API_KEY}&url=${url}&lang=en`)
+    .then(res=> res.json())
+    .then(data=>{
+        console.log("data: ", data)
+        document.getElementById('agreement').innerHTML = "Agreement : " + data.agreement
+                document.getElementById('confidence').innerHTML = "Confidence : " + data.confidence
+                document.getElementById('irony').innerHTML = "Irony:  : " + data.irony
+                document.getElementById('model').innerHTML = "Model : " + data.model
+                document.getElementById('score_tag').innerHTML = "Score Tag : " + data.score_tag
+    }).catch(error => console.log('error', error));
+    console.log("https://api.meaningcloud.com/sentiment-2.1?"+`key=${ process.env.API_KEY}&url=${url}&lang=en`)
+    // fetch("https://api.meaningcloud.com/sentiment-2.1?"+`key=${ process.env.API_KEY}&url=${url}&lang=en`)
+    //     .then(response => (response.json()))
+    //     .then((res) => ( function (res) {
+    //             console.log("client side res: ", res)
+    //             document.getElementById('agreement').innerHTML = "Agreement : " + res.agreement
+    //             document.getElementById('confidence').innerHTML = "Confidence : " + res.confidence
+    //             document.getElementById('irony').innerHTML = "Irony:  : " + res.irony
+    //             document.getElementById('model').innerHTML = "Model : " + res.model
+    //             document.getElementById('score_tag').innerHTML = "Score Tag : " + res.score_tag
 
-            }
-        ))
-        .catch(error => console.log('error', error));
+    //         }
+    //     ))
+    //     .catch(error => console.log('error', error));
     // const postNewsLink = (url = '', data = { test: 'eeeeee' }) => {
     //     console.log("Data in postNewsLink: ", data);
     //     return fetch(url, {
